eaa4cdf6768c19056fbbbaf9590663835aac6e4f
H S MOHAMMED FARHAN <farhanhsthaha786@gmail.com>
2025-09-03 11:58:51 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,7 +0,0 @@
-// Toggle mobile menu
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('navMenu');
-
-hamburger.addEventListener('click', () => {
-  navMenu.querySelector('ul').classList.toggle('active');
-});
